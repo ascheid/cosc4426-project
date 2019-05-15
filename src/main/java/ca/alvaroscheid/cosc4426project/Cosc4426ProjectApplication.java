403a3de97939e0364d2f952bb48d05e3c85d137b
Alvaro Scheid <ascheid@gmail.com>
2019-05-15 18:03:25 -0300

    Project initialization

---
+++ src/main/java/ca/alvaroscheid/cosc4426project/Cosc4426ProjectApplication.java
@@ -0,0 +1,13 @@
+package ca.alvaroscheid.cosc4426project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cosc4426ProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Cosc4426ProjectApplication.class, args);
+	}
+
+}
